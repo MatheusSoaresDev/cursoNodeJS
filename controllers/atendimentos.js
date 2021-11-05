b8d667d9677e19d5e5a910a32b46663605c95e68
Matheus Soares <matheus_onlive@live.com>
2021-11-04 23:30:06 -0300

    Buscando todos os atendimentos e atendimento filtrado pelo id com o metodo GET.

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,7 +1,15 @@
 const Atendimento = require('../models/atendimento');
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Rota de atendimentos fazendo um get'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPorId(id, res)
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
