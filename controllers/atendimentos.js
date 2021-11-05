329a53f3dbb9095fc67d9ab3cc498058cbae2826
Matheus Soares <matheus_onlive@live.com>
2021-11-04 22:03:55 -0300

    Tratamento de erros na requisição adicionado.

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,9 +3,8 @@ const Atendimento = require('../models/atendimento');
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Rota de atendimentos fazendo um get'))
 
-    app.post('/atendimentos', (req, res)=> {
+    app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
-        Atendimento.adiciona(atendimento)
-        res.send('Rota de atendimentos fazendo um post')
+        Atendimento.adiciona(atendimento, res)
     })
 }
